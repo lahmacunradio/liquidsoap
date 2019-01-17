7b986b4bed4476d30ca176791e28ec5f9774e06b
bp <bokorpet@gmail.com>
2019-01-17 11:09:04 +0100

    Player page template added

---
+++ wp/lahma_maker/templates/page_player.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying centered pages.
+ *
+ * Template Name: Page player (Lahma)
+ *
+ * @package Maker
+ */
+
+get_header(); ?>
+
+<div id="main" class="site-main" role="main">
+	<div id="content" class="site-content">
+		<div id="primary" class="content-area">
+
+<iframe src="http://178.238.217.146:8083/public/boerek/embed" frameborder="0" allowtransparency="false" style="width: 100%; min-height: 50px; border: 0;"></iframe>
+		</div>
+
+		<?php get_sidebar(); ?>
+
+	</div><!-- #content -->
+</div><!-- #main -->
+
+<?php get_footer(); ?>
