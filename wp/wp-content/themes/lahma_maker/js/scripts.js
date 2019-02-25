704d6eaf8a399579b73892aeaee0bebfde5ce834
Gaspar <baxgas@gmail.com>
2019-02-25 19:10:35 +0100

    Modernizr include and hopefully safer mobile detection. Page center fix

--- wp/wp-content/themes/lahma_maker/js/scripts.js
+++ wp/wp-content/themes/lahma_maker/js/scripts.js
@@ -2,13 +2,28 @@
 
 var $ = jQuery.noConflict();
 
+/* Touch detect */
+var deviceAgent = navigator.userAgent.toLowerCase();
+var isTouchDevice = Modernizr.touch || (deviceAgent.match(/(iphone|ipod|ipad)/) || deviceAgent.match(/(android)/)  || deviceAgent.match(/(iemobile)/) || deviceAgent.match(/iphone/i) || deviceAgent.match(/ipad/i) || deviceAgent.match(/ipod/i) || deviceAgent.match(/blackberry/i) || deviceAgent.match(/bada/i));
 
 function is_touch_device() {
+/* 
 if(window.matchMedia("(any-pointer: coarse)").matches) {
     // touchscreen
     //console.log( "touch!" );
     $("html").addClass("ismobile");
 }
+ */
+
+if (isTouchDevice) {
+	//Do something touchy
+	$('a').off('hover');
+    $("html").addClass("ismobile");
+} else {
+	//Can't touch this
+	$("html").addClass("notmobile");
+} 
+ 
 }
 
 function escapeHtml(unsafe) {
