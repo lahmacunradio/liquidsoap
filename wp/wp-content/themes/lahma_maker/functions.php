70bc35e2244bed254cbeb67724fc3ed0513a4e2d
gammaw <bokorpet@gmail.com>
2019-05-03 12:06:13 +0200

    Delete open news item link
    News title is a link anyway

--- wp/wp-content/themes/lahma_maker/functions.php
+++ wp/wp-content/themes/lahma_maker/functions.php
@@ -61,12 +61,6 @@ function new_excerpt_more($more) {
 }
 add_filter('excerpt_more', 'new_excerpt_more', 21 );
 
-function the_excerpt_more_link( $excerpt ){
-    $post = get_post();
-    $excerpt .= '<a class="readmorelink" href="'. get_permalink($post->ID) . '">Open news item »</a>';
-    return $excerpt;
-}
-add_filter( 'the_excerpt', 'the_excerpt_more_link', 21 );
 
  function excerpt_length_home( $length ) {
  if ( is_front_page() || is_home() ) {
