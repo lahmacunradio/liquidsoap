de2a0036b495d09dd9526cecc8054ee7c0fb4a94
Gas Bax <baxgas@gmail.com>
2020-02-27 18:11:03 +0100

    added Jambikus

--- wp/wp-content/themes/lahma_maker/Showsschedule.php
+++ wp/wp-content/themes/lahma_maker/Showsschedule.php
@@ -1,84 +1,83 @@
 <div class="Monday day">
     <h3>Monday / Hétfő</h3>
     <ul>
-    <li>07:00–08:00 <strong><a href="../shows/reggeli-musor-szarok-bele/">reggeli műsor, szarok bele</a></strong> | <a href="https://www.mixcloud.com/reggelimusor/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>11:11–12:11 <strong><a href="../shows/transverszia/">transverszia</a></strong> | <a href="https://www.mixcloud.com/junsuzuki/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>13:00–14:00 <strong><a href="../shows/a-who-say/">A Who Say</a></strong> | <a href="http://www.mixcloud.com/A_Who_Say/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>14:00–16:00 <strong><a href="../shows/dalmata-radio/">Dalmata Radio</a></strong>
-    <li>17:00–18:00 <strong><a href="../shows/udcsi/">Üdcsi</a></strong> | <a href="https://www.mixcloud.com/%C3%9Cdcsi/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>18:00–19:00 <strong><a href="../shows/szmuti-csorba/">Szmúti csórba</a></strong> | <a href="https://www.mixcloud.com/baksa-gaspar" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>19:00–19:30 <strong><a href="../shows/kezek-a-sebekben/">Kezek a Sebekben</a></strong> | <a href="https://www.mixcloud.com/KezekaSebekben/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>20:00–22:00 <strong><a href="../shows/earth-oddities-erdenklang/">Earth Oddities – Erdenklang</a></strong> | <a href="https://soundcloud.com/a_leidal" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>22:00–24:00 <strong><a href="../shows/infinite-scroll/">Infinite Scroll</a></strong> | <a href="https://soundcloud.com/gilbertpomelo" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>07:00–08:00 <strong><a href="../shows/reggeli-musor-szarok-bele/">reggeli műsor, szarok bele</a></strong> | <a href="https://www.mixcloud.com/reggelimusor/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>11:11–12:11 <strong><a href="../shows/transverszia/">transverszia</a></strong> | <a href="https://www.mixcloud.com/junsuzuki/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>13:00–14:00 <strong><a href="../shows/a-who-say/">A Who Say</a></strong> | <a href="http://www.mixcloud.com/A_Who_Say/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>14:00–16:00 <strong><a href="../shows/dalmata-radio/">Dalmata Radio</a></strong>
+        <li>17:00–18:00 <strong><a href="../shows/udcsi/">Üdcsi</a></strong> | <a href="https://www.mixcloud.com/%C3%9Cdcsi/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>18:00–19:00 <strong><a href="../shows/szmuti-csorba/">Szmúti csórba</a></strong> | <a href="https://www.mixcloud.com/baksa-gaspar" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>19:00–19:30 <strong><a href="../shows/kezek-a-sebekben/">Kezek a Sebekben</a></strong> | <a href="https://www.mixcloud.com/KezekaSebekben/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>20:00–22:00 <strong><a href="../shows/earth-oddities-erdenklang/">Earth Oddities – Erdenklang</a></strong> | <a href="https://soundcloud.com/a_leidal" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>22:00–24:00 <strong><a href="../shows/infinite-scroll/">Infinite Scroll</a></strong> | <a href="https://soundcloud.com/gilbertpomelo" target="_blank" rel="noopener noreferrer">archive</a></li>
     </ul>
 </div>
 <div class="Tuesday day">
     <h3>Tuesday / Kedd</h3>
     <ul>
-    <li>03:04–05:30 <strong><a href="../shows/lahmacun-gyorsnaszad-hidja/">Lahmacun gyorsznaszád hídja</a></strong></li>
-    <li>13:00–14:30 <strong><a href="../shows/csinaltam-neked-egy-valogatas-kazettat/">Csináltam neked egy válogatás kazettát</a></strong> | <a href="https://www.mixcloud.com/vargyai/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>16:00–17:00 <strong><a href="../shows/mills-strange-weather-condition/">Mill’s Strange Weather Condition</a></strong></li>
-    <li>17:00–18:00 <strong><a href="../shows/azvlm/">AZVLM</a></strong></li>
-    <li>18:00–19:00 <strong><a href="../shows/mmn-radio/">MMN radio</a></strong> | <a href="https://www.mixcloud.com/mmmnmnm/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>19:30–20:30 <strong><a href="../shows/havizaj/">Havizaj</a></strong>  | <a href="https://www.mixcloud.com/havizaj/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>21:00–22:00 <strong><a href="../shows/turmeric-acid/">Turmeric Acid</a></strong> | <a href="https://www.mixcloud.com/Turmeric_Acid/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>03:04–05:30 <strong><a href="../shows/lahmacun-gyorsnaszad-hidja/">Lahmacun gyorsznaszád hídja</a></strong></li>
+        <li>13:00–14:30 <strong><a href="../shows/csinaltam-neked-egy-valogatas-kazettat/">Csináltam neked egy válogatás kazettát</a></strong> | <a href="https://www.mixcloud.com/vargyai/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>16:00–17:00 <strong><a href="../shows/mills-strange-weather-condition/">Mill’s Strange Weather Condition</a></strong></li>
+        <li>17:00–18:00 <strong><a href="../shows/azvlm/">AZVLM</a></strong></li>
+        <li>18:00–19:00 <strong><a href="../shows/mmn-radio/">MMN radio</a></strong> | <a href="https://www.mixcloud.com/mmmnmnm/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>19:30–20:30 <strong><a href="../shows/havizaj/">Havizaj</a></strong>  | <a href="https://www.mixcloud.com/havizaj/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>21:00–22:00 <strong><a href="../shows/turmeric-acid/">Turmeric Acid</a></strong> | <a href="https://www.mixcloud.com/Turmeric_Acid/" target="_blank" rel="noopener noreferrer">archive</a></li>
     </ul>
 </div>
 <div class="Wednesday day">
     <h3>Wednesday / Szerda</h3>
     <ul>
-    <li>12:00–14:00 <strong><a href="../shows/off-air-premiere/">OFF AIR Premiere</a>&nbsp;</strong></li>
-    <li>15:00–16:00 <strong><a href="../shows/erto-hallgatas/">Értő Hallgatás</a>&nbsp;</strong>| <a href="https://www.mixcloud.com/%C3%A9rt%C5%91hallgat%C3%A1s/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>16:00–17:00 <strong><a href="../shows/wikihow-adventure-cruise/">Wikihow Adventure Cruise</a></strong>&nbsp;| <a href="https://www.mixcloud.com/wikihowadventurecruise/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>18:00–19:00 <strong><a href="../shows/svadhyaya-onkutatolabor/">Svādhyāya - Önkutatólabor</a></strong>&nbsp;</li>
-    <li>19:00–20:00 <strong><a href="../shows/geigercounterculture/">Geigercounterculture</a></strong>&nbsp;| <a href="https://www.mixcloud.com/Geigercounterculture/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>20:00–21:00 <strong><a href="../shows/cellz-nocturnal-emissions/">@cellz nocturnal emissions</a></strong>&nbsp;| <a href="http://cellz.neocities.org/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>21:00–22:00 <strong><a href="../shows/eastern-daze-via-cashmere-radio/">Eastern Daze (via Cashmere Radio)</a></strong></li>
-    <li>23:00–24:00 <strong><a href="../shows/radio-bluszcze/">Radio Bluszcze (via Radio Kapitał)</a></strong></li>
-    </ul>
-
+        <li>12:00–14:00 <strong><a href="../shows/off-air-premiere/">OFF AIR Premiere</a>&nbsp;</strong></li>
+        <li>15:00–16:00 <strong><a href="../shows/erto-hallgatas/">Értő Hallgatás</a>&nbsp;</strong>| <a href="https://www.mixcloud.com/%C3%A9rt%C5%91hallgat%C3%A1s/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>16:00–17:00 <strong><a href="../shows/wikihow-adventure-cruise/">Wikihow Adventure Cruise</a></strong>&nbsp;| <a href="https://www.mixcloud.com/wikihowadventurecruise/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>18:00–19:00 <strong><a href="../shows/svadhyaya-onkutatolabor/">Svādhyāya - Önkutatólabor</a></strong>&nbsp;</li>
+        <li>19:00–20:00 <strong><a href="../shows/geigercounterculture/">Geigercounterculture</a></strong>&nbsp;| <a href="https://www.mixcloud.com/Geigercounterculture/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>20:00–21:00 <strong><a href="../shows/cellz-nocturnal-emissions/">@cellz nocturnal emissions</a></strong>&nbsp;| <a href="http://cellz.neocities.org/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>21:00–22:00 <strong><a href="../shows/eastern-daze-via-cashmere-radio/">Eastern Daze (via Cashmere Radio)</a></strong></li>
+        <li>23:00–24:00 <strong><a href="../shows/radio-bluszcze/">Radio Bluszcze (via Radio Kapitał)</a></strong></li>
     </ul>
 </div>
 <div class="Thursday day">
     <h3>Thursday / Csütörtök</h3>
     <ul>
-    <li>14:00–15:00 <strong><a href="../shows/muhelycast/">Műhelycast</a></strong></li>
-    <li>15:00–16:00 <strong><a href="../shows/konkret-riport/">Konkrét Riport</a></strong>&nbsp;| <a href="https://www.mixcloud.com/ZzzzBone/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>16:00–17:00 <strong><a href="../shows/alternoon/">AlternoON</a></strong></li>
-    <li>17:00–19:00 <strong><a href="../shows/lazy-calm-raga/">Lazy Calm Raga</a></strong>&nbsp;| <a href="https://www.mixcloud.com/lazycalmraga/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>19:00–20:00 <strong><a href="../shows/dalmata-gergo-show/">Dalmata Gergő Show</a></strong> | <a href="https://www.mixcloud.com/djluci/" target="_blank" rel="noopener noreferrer">archive</a> </li>
-    <li>20:00–21:00 <strong><a href="../shows/dodo/">dôdô</a></strong> | <a href="https://soundcloud.com/george-finlay-ramsay/best-of-god" target="_blank" rel="noopener noreferrer">archive</a> </li>
-    <li>21:00–23:00 <strong><a href="../shows/great-galactic-ghoul/">Great Galactic Ghoul</a></strong>&nbsp;| <a href="https://www.mixcloud.com/GREATGALACTICGHOUL/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>23:00–00:00 <strong><a href="../shows/gyogyfurdo/">Gyógyfürdő</a></strong></li>
+        <li>14:00–15:00 <strong><a href="../shows/muhelycast/">Műhelycast</a></strong></li>
+        <li>15:00–16:00 <strong><a href="../shows/konkret-riport/">Konkrét Riport</a></strong>&nbsp;| <a href="https://www.mixcloud.com/ZzzzBone/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>16:00–17:00 <strong><a href="../shows/alternoon/">AlternoON</a></strong></li>
+        <li>17:00–19:00 <strong><a href="../shows/lazy-calm-raga/">Lazy Calm Raga</a></strong>&nbsp;| <a href="https://www.mixcloud.com/lazycalmraga/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>19:00–20:00 <strong><a href="../shows/dalmata-gergo-show/">Dalmata Gergő Show</a></strong> | <a href="https://www.mixcloud.com/djluci/" target="_blank" rel="noopener noreferrer">archive</a> </li>
+        <li>20:00–21:00 <strong><a href="../shows/dodo/">dôdô</a></strong> | <a href="https://soundcloud.com/george-finlay-ramsay/best-of-god" target="_blank" rel="noopener noreferrer">archive</a> </li>
+        <li>21:00–23:00 <strong><a href="../shows/great-galactic-ghoul/">Great Galactic Ghoul</a></strong>&nbsp;| <a href="https://www.mixcloud.com/GREATGALACTICGHOUL/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>23:00–00:00 <strong><a href="../shows/gyogyfurdo/">Gyógyfürdő</a></strong></li>
     </ul>
 </div>
 <div class="Friday day">
     <h3>Friday / Péntek</h3>
     <ul>
-    <li>16:00–17:00 <strong><a href="../shows/user-f-3901-music-idm/">/user/f-3901/music/idm |</a></strong>| <a href="https://www.mixcloud.com/user_f-3901_music_idm/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>17:00–18:00 <strong><a href="../shows/dead-hound-radio/">Dead Hound</a></strong></li>
-    <li>18:00–19:00 <strong><a href="../shows/agybaj/">Agybaj</a></strong></li>
-    <li>19:00–21:00 <strong><a href="../shows/mukodo-mukod/">Működő Működ</a></strong>| <a href="https://www.mixcloud.com/d-j-k-o-d/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>21:00-23:00 <strong><a href="../shows/arcade-ok-alatt/">Arcade-ok alatt</a></strong></li>
+        <li>16:00–17:00 <strong><a href="../shows/user-f-3901-music-idm/">/user/f-3901/music/idm |</a></strong>| <a href="https://www.mixcloud.com/user_f-3901_music_idm/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>17:00–18:00 <strong><a href="../shows/dead-hound-radio/">Dead Hound</a></strong></li>
+        <li>18:00–19:00 <strong><a href="../shows/agybaj/">Agybaj</a></strong></li>
+        <li>19:00–21:00 <strong><a href="../shows/mukodo-mukod/">Működő Működ</a></strong>| <a href="https://www.mixcloud.com/d-j-k-o-d/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>21:00-23:00 <strong><a href="../shows/arcade-ok-alatt/">Arcade-ok alatt</a></strong></li>
     </ul>
 </div>
 <div class="Saturday day">
     <h3>Saturday / Szombat</h3>
     <ul>
-    <li>15:00–16:30 <strong><a href="../shows/learning-audio-classification-with-torso/">Learning Audio Classification with Torso</a></strong></li>
-    <li>17:00–18:00 <strong><a href="../shows/footwork-jimbob/">Footwork Jimbob</a></strong>| <a href="https://www.mixcloud.com/regost542/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>18:00–20:00 <strong><a href="../shows/rnr666-radioshow/">RNR666 radioshow</a></strong>| <a href="https://www.mixcloud.com/RNR666/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>20:00–22:00 <strong><a href="../shows/mood-sequence/">Mood Sequence</a></strong></li>
-    <li>22:30–23:30 <strong><a href="../shows/brains-sublimating/">Brains Sublimating</a></strong>| <a href="https://www.mixcloud.com/tapes_sublimating/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>13:30-14:30 <strong><a href="../shows/jambikus-nesz/">Jambikus nesz</a></strong></li>
+        <li>15:00–16:30 <strong><a href="../shows/learning-audio-classification-with-torso/">Learning Audio Classification with Torso</a></strong></li>
+        <li>17:00–18:00 <strong><a href="../shows/footwork-jimbob/">Footwork Jimbob</a></strong>| <a href="https://www.mixcloud.com/regost542/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>18:00–20:00 <strong><a href="../shows/rnr666-radioshow/">RNR666 radioshow</a></strong>| <a href="https://www.mixcloud.com/RNR666/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>20:00–22:00 <strong><a href="../shows/mood-sequence/">Mood Sequence</a></strong></li>
+        <li>22:30–23:30 <strong><a href="../shows/brains-sublimating/">Brains Sublimating</a></strong>| <a href="https://www.mixcloud.com/tapes_sublimating/" target="_blank" rel="noopener noreferrer">archive</a></li>
     </ul>
 </div>
 <div class="Sunday day">
     <h3>Sunday / Vasárnap</h3>
     <ul>
-    <li>12:00–14:00 Bambuszrádió</li>
-    <li>14:00–18:00 <strong><a href="../shows/donald-kacsa-klub/">Donald Kacsa Klub</a></strong> | <a href="https://www.facebook.com/donaldkacsaklub/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>18:30–19:30 <strong><a href="../shows/hetedik-tipusu-talalkozas/">Hetedik Típusú Találkozás</a></strong> | <a href="https://www.mixcloud.com/HetedikTipusuTalalkozas/" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>20:00–21:00 <strong><a href="../shows/speakers-corner/"> Speakers Corner</a></strong> | <a href="https://anchor.fm/speakers-corner" target="_blank" rel="noopener noreferrer">archive</a></li>
-    <li>21:00–22:00 <strong><a href="../shows/sub-burek/">Sub Burek</a></strong></li>
+        <li>12:00–14:00 Bambuszrádió</li>
+        <li>14:00–18:00 <strong><a href="../shows/donald-kacsa-klub/">Donald Kacsa Klub</a></strong> | <a href="https://www.facebook.com/donaldkacsaklub/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>18:30–19:30 <strong><a href="../shows/hetedik-tipusu-talalkozas/">Hetedik Típusú Találkozás</a></strong> | <a href="https://www.mixcloud.com/HetedikTipusuTalalkozas/" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>20:00–21:00 <strong><a href="../shows/speakers-corner/"> Speakers Corner</a></strong> | <a href="https://anchor.fm/speakers-corner" target="_blank" rel="noopener noreferrer">archive</a></li>
+        <li>21:00–22:00 <strong><a href="../shows/sub-burek/">Sub Burek</a></strong></li>
     </ul>
 </div>
