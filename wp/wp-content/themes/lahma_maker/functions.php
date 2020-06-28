9f0d45cfc62be3ceef7967c7dc9550108b6b13c7
Gas Bax <baxgas@gmail.com>
2020-06-28 16:11:11 +0200

    #224 remove menu walker and cleanup code

--- wp/wp-content/themes/lahma_maker/functions.php
+++ wp/wp-content/themes/lahma_maker/functions.php
@@ -229,60 +229,6 @@ if ( $contShower == "show" ) {
 
 }/* End Lahma Donate menu */
 
-/* Menu post image */
-class Menu_With_Description extends Walker_Nav_Menu {
-	function start_el( &$output, $item, $depth, $args ) {
-		global $wp_query;
-		$indent = ( $depth ) ? str_repeat( "", $depth ) : '';
-		$class_names = $value = '';
-		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
-		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) );
-		$class_names = ' class="' . esc_attr( $class_names ) . '"';
-		$output .= $indent . '<li id="menu-item-' . $item->ID . '"' . $value . $class_names . '>';
-		$attributes = ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) . '"' : '';
-		$attributes .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) . '"' : '';
-		$attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) . '"' : '';
-		$attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) . '"' : '';
-// get user defined attributes for thumbnail images
-		$attr_defaults = array(
-			'class' => 'nav_thumb',
-			'alt'   => esc_attr( $item->attr_title ),
-            'title' => esc_attr( $item->attr_title )
-		);
-		$attr          = isset( $args->thumbnail_attr ) ? $args->thumbnail_attr : '';
-		$attr          = wp_parse_args( $attr, $attr_defaults );
-		$item_output = $args->before;
-// thumbnail image output
-        $item_output .= '<a href="' . esc_attr( $item->url ) . '"';
-        $item_output .= isset($item->target) ? ' target="' . esc_attr( $item->target ) . '"' : '';
-        $item_output .= ' class="nav-post-link">';
-		$item_output .= ( isset( $args->thumbnail_link ) && $args->thumbnail_link ) ? '<a' . $attributes . '>' : '';
-		$item_output .= apply_filters( 'menu_item_thumbnail', ( isset( $args->thumbnail ) && $args->thumbnail ) ? get_the_post_thumbnail( $item->object_id, ( isset( $args->thumbnail_size ) ) ? $args->thumbnail_size : 'thumbnail', $attr ) : '', $item, $args, $depth );
-		$item_output .= ( isset( $args->thumbnail_link ) && $args->thumbnail_link ) ? '</a>' : '';
-// menu link output
-		$item_output .= '<div class="nav-text">';
-		$item_output .= $args->link_before . '<span class="maintitle">' . apply_filters( 'the_title', $item->title, $item->ID ) . '</span>' . $args->link_after;
-// menu description output based on depth
-		$item_output .= ( $args->desc_depth >= $depth ) ? '<span class="sub">' . $item->description . '</span>' : '';
-// close menu link anchor
-		$item_output .= '</div></a>';
-		$item_output .= $args->after;
-		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
-	}
-}
-
-add_filter( 'wp_nav_menu_args', 'my_add_menu_descriptions' );
-function my_add_menu_descriptions( $args ) {
-	if ( $args['theme_location'] == 'primary' ) {
-		$args['walker']         = new Menu_With_Description;
-		$args['desc_depth']     = 1;
-		$args['thumbnail']      = true;
-		$args['thumbnail_link'] = false;
-		$args['thumbnail_size'] = 'nav_thumb';
-		$args['thumbnail_attr'] = array( 'class' => 'nav_thumb my_thumb' );
-	}
-	return $args;
-}
 
 /* Add Social Menu */
 register_nav_menus( array(
--- wp/wp-content/themes/lahma_maker/js/scripts.js
+++ wp/wp-content/themes/lahma_maker/js/scripts.js
@@ -7,13 +7,6 @@ var deviceAgent = navigator.userAgent.toLowerCase();
 var isTouchDevice = Modernizr.touch || (deviceAgent.match(/(iphone|ipod|ipad)/) || deviceAgent.match(/(android)/)  || deviceAgent.match(/(iemobile)/) || deviceAgent.match(/iphone/i) || deviceAgent.match(/ipad/i) || deviceAgent.match(/ipod/i) || deviceAgent.match(/blackberry/i) || deviceAgent.match(/bada/i));
 
 function is_touch_device() {
-/*
-if(window.matchMedia("(any-pointer: coarse)").matches) {
-    // touchscreen
-    //console.log( "touch!" );
-    $("html").addClass("ismobile");
-}
- */
 
 if (isTouchDevice) {
 	//Do something touchy
@@ -28,14 +21,6 @@ if (isTouchDevice) {
 
 function escapeHtml(unsafe) {
     return unsafe
-         /*
-         .replace(/&/g, "&amp;")
-         .replace(/</g, "&lt;")
-         .replace(/>/g, "&gt;")
-         .replace(/"/g, "&quot;")
-         .replace(/'/g, "&#039;")
-         .replace(/–/g, "&#8211;");
-         */
          .replace("&#8211;", "—")
          .replace("&amp;", "and")
          .replace("&lt;", "-")
@@ -70,7 +55,6 @@ function swipeboxGalleryFixer() {
 function set_home_class(scope){
     if (scope.pathname == "/"  &&  scope.hash.length <= 1  &&  scope.search.length <= 1) {
         jQuery("body").addClass("home");
-        // console.log("home");
     } else {
         jQuery("body").removeClass("home");
     }
@@ -90,7 +74,6 @@ function makeAlbumPictureClick() {
 // A $( document ).ready() block.
 $( document ).ready(function() {
 
-    // console.log( "ready!" );
     is_touch_device();
     openAllExternalBlank();
     swipeboxGalleryFixer();
@@ -117,8 +100,6 @@ $( document ).ready(function() {
     /* AJAX link click */
     jQuery(document).on("click", "#page a[target!='_blank']:not(a[href^='#']):not(.swipebox):not(.avoidAjax)", function(e){
         var link = jQuery(this).attr("href");
-        // var title = jQuery(responseHtml).filter('title').text();
-        // console.log(link);
 
         jQuery("#main").load( link + " #primary", function(responseText) {
             var newtitle = escapeHtml(responseText.match(/<title>([^<]*)/)[1]);
@@ -126,7 +107,6 @@ $( document ).ready(function() {
             openAllExternalBlank();
             swipeboxGalleryFixer();
             makeAlbumPictureClick();
-            //$("#site-navigation-toggle.toggled").trigger("click"); //automatically collapses open show menu on mobile
             $("#primary-menu .sub-menu").slideUp("fast", function() {
                 $(this).addClass("closed");
             });
@@ -135,18 +115,10 @@ $( document ).ready(function() {
         );
         e.preventDefault();
         history.pushState({}, null, link);
-        // jQuery(document).find("title").text(jQuery(responseHtml).filter('title').text());
         set_home_class(document.location);
         jQuery(".main-navigation ul.menu li:hover > ul").hide();
 
     });
-
-
-
-    /*jQuery(document).on("click", ".site-title a", function(e){
-        jQuery("body").addClass("home");
-    });*/
-
 
 // Main-menu toggler
 $(document).on("click", "html.ismobile nav.main-navigation li.menu-item-has-children a", function(e){
@@ -167,16 +139,11 @@ var dateobj = new Date();
 var ndateobj = dateobj.getDay() || 8 - 1;
 var gooddateobj = ndateobj - 1;
 var datedifference = 7 - gooddateobj;
-		// console.log(ndateobj);
-		// console.log(gooddateobj);
-		// console.log(datedifference);
 
 window.onfocus = function() {
 		var Cdateobj = new Date();
 		var Cndateobj = Cdateobj.getDay() || 8 - 1;
 		var Cgooddateobj = Cndateobj - 1;
-		// console.log(gooddateobj);
-		// console.log(Cgooddateobj);
 
     if ( Cgooddateobj !== gooddateobj && !$("body").hasClass("Playing") ) {
         location.reload();
@@ -184,14 +151,9 @@ window.onfocus = function() {
 
 };
 
-// var $monday = $(".day").eq(0);
-// var $lastday = $(".day").eq(6);
-// var $today = $(".day").eq(gooddateobj);
-
 function sortDates( callbackFunction ) {
 	$(".day").not(".sorted").addClass("notsorted");
 	$(".day.notsorted").each(function(i){
-        // console.log(i);
         if ( i < gooddateobj ) {
             $(this).appendTo($("#endofweek"));
         }
@@ -220,10 +182,6 @@ function dateWriteSchedule() {
 
 }
 
-// var nextday = new Date(dateobj.getFullYear(),dateobj.getMonth(),dateobj.getDate()+1);
-// console.log(nextday);
-
-
 $( document ).on("ajaxComplete", function(){
 	sortDates( dateWriteSchedule );
 });
@@ -237,12 +195,6 @@ $(document).on("click", ".day > h3", function(){
     $(this).toggleClass("openedSchedule").next("ul").slideToggle();
 });
 
-
-// prevent Shows menu clicking
-/*
-$(".menu-lahma-menu-container li.menu-item-has-children > a").removeAttr("href").css({'cursor': 'pointer'}).addClass("avoidAjax");
-*/
-
 // Close Donate
 $(document).on("click", "#donatebanner a#closedonate", function(e){
 	$(this).hide();
@@ -252,7 +204,6 @@ $(document).on("click", "#donatebanner a#closedonate", function(e){
 
 // Donate Campaigns
 let progresswidth = $( "#donatebanner .campaign-progress-bar" ).attr( "aria-valuenow" );
-// console.log(progresswidth);
 $("#donatebanner .goalprogress").text(Math.round(progresswidth*100)/100 + "%")
 });
 
