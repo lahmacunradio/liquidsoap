862bd895b84b5b2e5816a063380ec0600ac1bb55
baxgas <baxgas@gmail.com>
2020-09-30 11:32:32 +0200

    #254 cleanup dummy

--- wp/wp-content/themes/lahma_maker/template-parts/content-arcsi.php
+++ wp/wp-content/themes/lahma_maker/template-parts/content-arcsi.php
@@ -12,44 +12,6 @@
 <h3>Arcsived shows for <?php the_title(); ?></h3>
 
 <?php
-$showarcsiJSON = 
-'[
-    {
-        "id" : 123456,
-        "number" : 1,
-        "archived" : true,
-        "image_url" : "https://www.lahmacun.hu/wp-content/uploads/95501220_1681612051978410_2631130930130976768_n-300x285.jpg",
-        "play_date" : "2020-06-01",
-        "name" : "First Episode",
-        "description" : "Lovely first episode",
-        "download_link" : "https://our.rad.io/arcsi/item/1/download",        
-        "download_count" : "5"       
-    },
-    {
-        "id" : 56789,
-        "number" : 2,
-        "archived" : true,
-        "image_url" : "https://www.lahmacun.hu/wp-content/uploads/lahma_merites_EP01-300x330.png",
-        "play_date" : "2020-06-11",
-        "name" : "Second Episode",
-        "description" : "Lovely second episode",
-        "download_link" : "https://our.rad.io/arcsi/item/2/download",
-        "download_count" : "10"
-    },
-    {
-        "id" : 987564,
-        "number" : 3,
-        "archived" : true,
-        "image_url" : "https://www.lahmacun.hu/wp-content/uploads/HAVIZAJ-300x136.jpg",
-        "play_date" : "2020-08-18",
-        "name" : "Third Episode",
-        "description" : "Lovely third episode",
-        "download_link" : "https://our.rad.io/arcsi/item/3/download",
-        "download_count" : "2"        
-    }
-]';
-
-// $showarcsi = json_decode($showarcsiJSON, true);
 
 $showslug = get_post_field( 'post_name', get_post() );
 $showjson = file_get_contents('https://arcsi.lahmacun.hu/arcsi/show/' . $showslug . '/archive');
