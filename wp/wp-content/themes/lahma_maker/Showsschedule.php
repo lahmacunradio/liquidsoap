1c959b47f9ed4dafb210414dd0664c8aa3929cfb
pvj <alavela05@gmail.com>
2020-11-12 02:04:32 +0100

    front-end - put new show boombap on schedule

--- wp/wp-content/themes/lahma_maker/Showsschedule.php
+++ wp/wp-content/themes/lahma_maker/Showsschedule.php
@@ -54,6 +54,7 @@
 <div class="Friday day">
     <h3>Friday / Péntek</h3>
     <ul>
+        <li>15:00-16:00 <strong><a href="../shows/boombap/">BoomBAP</a></strong></li>
         <li>16:00–17:00 <strong><a href="../shows/recently-played-music/">Recently Played Music</a></strong>| <a href="https://www.mixcloud.com/user_f-3901_music_idm/" target="_blank" rel="noopener">archive</a></li>
         <li>17:00–18:00 <strong><a href="../shows/dead-hound-radio/">Dead Hound</a></strong></li>
         <li>18:00–19:00 <strong><a href="../shows/agybaj/">Agybaj</a></strong></li>
