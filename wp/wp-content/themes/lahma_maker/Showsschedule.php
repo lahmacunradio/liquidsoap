2b3b10891dc6c3203532429ccdbc62a7fd941201
gammaw <bokorpet@gmail.com>
2020-11-16 22:15:04 +0100

    Fix some archive links
    #324

--- wp/wp-content/themes/lahma_maker/Showsschedule.php
+++ wp/wp-content/themes/lahma_maker/Showsschedule.php
@@ -56,8 +56,8 @@
     <ul>
         <li>15:30-16:00 <strong><a href="../shows/rodina-fektory/">Rodina Féktory</a></strong></li>
         <li>15:00-16:00 <strong><a href="../shows/boombap/">BoomBAP</a></strong></li>
-        <li>16:00–17:00 <strong><a href="../shows/recently-played-music/">Recently Played Music</a></strong>| <a href="https://www.mixcloud.com/user_f-3901_music_idm/" target="_blank" rel="noopener">archive</a></li>
-        <li>17:00–18:00 <strong><a href="../shows/dead-hound-radio/">Dead Hound</a></strong></li>
+        <li>16:00–17:00 <strong><a href="../shows/recently-played-music/">Recently Played Music</a></strong>| <a href="https://www.mixcloud.com/recently-played-music/" target="_blank" rel="noopener">archive</a></li>
+        <li>17:00–18:00 <strong><a href="../shows/dead-hound-radio/">Dead Hound</a></strong>| <a href="https://www.mixcloud.com/DeadHoundRadio/" target="_blank" rel="noopener">archive</a></li>
         <li>18:00–19:00 <strong><a href="../shows/agybaj/">Agybaj</a></strong></li>
         <li>19:00–21:00 <strong><a href="../shows/mukodo-mukod/">Működő Működ</a></strong>| <a href="https://www.mixcloud.com/d-j-k-o-d/" target="_blank" rel="noopener noreferrer">archive</a></li>
         <li>21:00-23:00 <strong><a href="../shows/arcade-ok-alatt/">Arcade-ok alatt</a></strong></li>
