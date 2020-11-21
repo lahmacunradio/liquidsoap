4b9563f7ea3b752b9e997ef8dcf47d62e727be00
baxgas <baxgas@gmail.com>
2020-11-21 19:37:31 +0100

    Arcsi player implement

--- wp/wp-content/themes/lahma_maker/template-parts/content-arcsi.php
+++ wp/wp-content/themes/lahma_maker/template-parts/content-arcsi.php
@@ -46,17 +46,13 @@ foreach($showarcsi as $archiveitem) {
         <div>Episode nr. <?php echo $shownumber; ?> – <span>Aired on <?php echo $showplaydate; ?></span> </div>
         <h4><?php echo $showname; ?></h4>   
         <p><?php echo $showdescription; ?></p> 
-        <div id="arcsi-audio-<?php echo $showid; ?>">
+        <div id="arcsi-audio-<?php echo $showid; ?>" class="arcsicontrols">
             <a class="arcsibutton arcsidown" href="<?php echo $server; ?>/arcsi/item/<?php echo $showid; ?>/download" target="_blank">
                 <i class="fa fa-download" aria-hidden="true"></i> Download
             </a>              
             <a class="arcsibutton arcsilisten avoidAjax" href="<?php echo $server; ?>/arcsi/item/<?php echo $showid; ?>">
                 <i class="fa fa-headphones" aria-hidden="true"></i> Listen
             </a>
-            <audio controls class="hiddenelement">
-                <source src="" type="audio/mpeg">
-                Your browser does not support the audio tag.
-            </audio>
         </div>
     </div>
 </div>
@@ -75,25 +71,48 @@ $( document ).ready(function() {
 
 let audioLink = null
 
+function stopAllAudio() {
+    let sounds = document.getElementsByTagName('audio');
+    for(i=0; i<sounds.length; i++) sounds[i].pause();
+    $("audio.episodeplay").remove()
+}
+
     jQuery(document).on("click", ".arcsilisten", function(e) {
         
         e.preventDefault();
-        let listenLink = $(this).attr('href')
-        // let listenLink = 'https://devarcsi.lahmacun.hu/arcsi/item/7'
-        // let listenLink = 'https://jsonplaceholder.typicode.com/todos/1'
+        // let listenLink = $(this).attr('href')
+        let listenLink = 'https://devarcsi.lahmacun.hu/arcsi/item/7'
+        // let listenLink = 'https://streaming.lahmacun.hu/api/nowplaying/1'
+
+        /* dummy audio link */
         audioLink="https://geekanddummy.com/wp-content/uploads/2014/02/ambient-noise-server-room.mp3"
 
-        console.log(listenLink)
+        // console.log(listenLink)
 
         let parentId = $(this).parent("div").attr("id")
-        console.log(parentId)
+        //console.log(parentId)
 
         fetch(listenLink) 
             .then(response => response.json())
-            .then(json => console.log(json))
-            .then(
-                $("#" + parentId + " audio").removeClass("hiddenelement").find("source").attr("src", audioLink)
-            );
+            .then(json => {
+                // console.log(json)
+                audioLink = json.file_url // get audio link from ARCSI json
+                // audioLink = json.station.listen_url // get DUMMY audio link from STATION json
+            } )
+            .then( () => { 
+                stopAllAudio()
+                $(".arcsicontrols a").removeClass("hiddenelement")
+                let $player = `
+                    <audio controls class="episodeplay">
+                        <source src="${audioLink}" type="audio/mpeg">
+                        Your browser does not support the audio tag.
+                    </audio>
+                `
+                $("#" + parentId).append($player)
+                $("#" + parentId + " > a").addClass("hiddenelement")
+            }
+            )
+            .catch( error => console.log(error) )
     
     });
 });
